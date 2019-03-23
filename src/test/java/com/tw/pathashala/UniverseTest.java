d489ea3c8121a6d118f2d91a1eb456516b939d38
Pulkit Gupta <pulkit.gupta@MacBookPro-wifi.local>
2019-03-23 20:21:25 +0530

    [assignment-02] add third passing test case

--- src/test/java/com/tw/pathashala/UniverseTest.java
+++ src/test/java/com/tw/pathashala/UniverseTest.java
@@ -100,6 +100,47 @@ class UniverseTest {
                 }
             }
         }
+    }
 
+    private void createThreeExpectedCellAlive(List<Cell> expectedCellsAlive) {
+        expectedCellsAlive.add(new Cell(1, 0, 1));
+        expectedCellsAlive.add(new Cell(1, 1, 1));
+        expectedCellsAlive.add(new Cell(1, 2, 1));
+    }
+
+    private void createGridAndThreeInputSeeds(List<List<Cell>> grid, List<Cell> seeds) {
+        seeds.add(new Cell(0, 1, 1));
+        seeds.add(new Cell(1, 1, 1));
+        seeds.add(new Cell(2, 1, 1));
+
+        for (int i = 0; i <= 3; i++) {
+            grid.add(new LinkedList<>());
+            for (int j = 0; j <= 3; j++) {
+                grid.get(i).add(new Cell(i, j, 0));
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Find generation when five seeds are provided with coordinates {1,0} {1,1} {1,2}")
+    void shouldFindNextGenerationWhenThreeSeedsAreProvided() {
+        Universe universe = new Universe();
+        List<List<Cell>> grid = universe.getGrid();
+        List<Cell> seeds = universe.getSeeds();
+        List<Cell> expectedCellsAlive = new LinkedList<>();
+        createGridAndThreeInputSeeds(grid, seeds);
+        createThreeExpectedCellAlive(expectedCellsAlive);
+        universe.provideSeed(grid, seeds);
+        int k = 0;
+
+        universe.nextGeneration(grid);
+
+        for (List<Cell> cells : grid) {
+            for (Cell cell : cells) {
+                if (cell.getStatus() == 1) {
+                    assertEquals(cell, expectedCellsAlive.get(k++));
+                }
+            }
+        }
     }
 }
