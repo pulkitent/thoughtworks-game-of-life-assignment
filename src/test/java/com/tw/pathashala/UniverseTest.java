6a7c856659737c98bfdd425ad51d027d7bba859a
Pulkit Gupta <pulkit.gupta@MacBookPro-wifi.local>
2019-03-23 20:17:23 +0530

    [assignment-02] add second passing test case

--- src/test/java/com/tw/pathashala/UniverseTest.java
+++ src/test/java/com/tw/pathashala/UniverseTest.java
@@ -10,7 +10,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UniverseTest {
 
-    private void createGridAndInputSeeds(List<List<Cell>> grid, List<Cell> seeds) {
+    private void createFourExpectedCellAlive(List<Cell> expectedCellsAlive) {
+        expectedCellsAlive.add(new Cell(1, 1, 1));
+        expectedCellsAlive.add(new Cell(1, 2, 1));
+        expectedCellsAlive.add(new Cell(2, 1, 1));
+        expectedCellsAlive.add(new Cell(2, 2, 1));
+    }
+
+    private void createGridAndFourInputSeeds(List<List<Cell>> grid, List<Cell> seeds) {
         seeds.add(new Cell(1, 1, 1));
         seeds.add(new Cell(1, 2, 1));
         seeds.add(new Cell(2, 1, 1));
@@ -24,23 +31,16 @@ class UniverseTest {
         }
     }
 
-    private void createExpectedCellAlive(List<Cell> expectedCellsAlive) {
-        expectedCellsAlive.add(new Cell(1, 1, 1));
-        expectedCellsAlive.add(new Cell(1, 2, 1));
-        expectedCellsAlive.add(new Cell(2, 1, 1));
-        expectedCellsAlive.add(new Cell(2, 2, 1));
-    }
-
 
     @Test
-    @DisplayName("Find generation when seed is provided to universe with coordinates {1,1} {1,2} {2,1} {2,2}")
-    void shouldFindNextGenerationWhenSeedIsProvided() {
+    @DisplayName("Find generation when four seeds are provided with coordinates {1,1} {1,2} {2,1} {2,2}")
+    void shouldFindNextGenerationWhenFourSeedsAreProvided() {
         Universe universe = new Universe();
         List<List<Cell>> grid = universe.getGrid();
         List<Cell> seeds = universe.getSeeds();
         List<Cell> expectedCellsAlive = new LinkedList<>();
-        createGridAndInputSeeds(grid, seeds);
-        createExpectedCellAlive(expectedCellsAlive);
+        createGridAndFourInputSeeds(grid, seeds);
+        createFourExpectedCellAlive(expectedCellsAlive);
         universe.provideSeed(grid, seeds);
         int k = 0;
 
@@ -56,4 +56,50 @@ class UniverseTest {
 
     }
 
+    private void createFiveExpectedCellAlive(List<Cell> expectedCellsAlive) {
+        expectedCellsAlive.add(new Cell(0, 1, 1));
+        expectedCellsAlive.add(new Cell(0, 2, 1));
+        expectedCellsAlive.add(new Cell(1, 0, 1));
+        expectedCellsAlive.add(new Cell(1, 2, 1));
+        expectedCellsAlive.add(new Cell(2, 1, 1));
+    }
+
+    private void createGridAndFiveInputSeeds(List<List<Cell>> grid, List<Cell> seeds) {
+        seeds.add(new Cell(0, 1, 1));
+        seeds.add(new Cell(0, 2, 1));
+        seeds.add(new Cell(1, 0, 1));
+        seeds.add(new Cell(1, 2, 1));
+        seeds.add(new Cell(2, 1, 1));
+
+        for (int i = 0; i <= 3; i++) {
+            grid.add(new LinkedList<>());
+            for (int j = 0; j <= 3; j++) {
+                grid.get(i).add(new Cell(i, j, 0));
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Find generation when five seeds are provided with coordinates {0,1} {0,2} {1,0} {1,2} {2,1}")
+    void shouldFindNextGenerationWhenFiveSeedsAreProvided() {
+        Universe universe = new Universe();
+        List<List<Cell>> grid = universe.getGrid();
+        List<Cell> seeds = universe.getSeeds();
+        List<Cell> expectedCellsAlive = new LinkedList<>();
+        createGridAndFiveInputSeeds(grid, seeds);
+        createFiveExpectedCellAlive(expectedCellsAlive);
+        universe.provideSeed(grid, seeds);
+        int k = 0;
+
+        universe.nextGeneration(grid);
+
+        for (List<Cell> cells : grid) {
+            for (Cell cell : cells) {
+                if (cell.getStatus() == 1) {
+                    assertEquals(cell, expectedCellsAlive.get(k++));
+                }
+            }
+        }
+
+    }
 }
